6f7317481f46f4fe2614d4f581193f4a04813219
Markus Knutsson <markus.knutsson@tweety.se>
2025-08-08 11:23:13 +0200

    Reflexion fw tested

--- keyboards/tweetydabird/reflexion/config.h
+++ keyboards/tweetydabird/reflexion/config.h
@@ -8,4 +8,13 @@
 #define SERIAL_USART_RX_PIN GP10
 #define SERIAL_USART_PIN_SWAP
 
-#define USB_VBUS_PIN GP27
+#define I2C_DRIVER I2CD0
+#define I2C1_SDA_PIN GP4
+#define I2C1_SCL_PIN GP5
+
+#define USB_VBUS_PIN GP2
+
+/* RP2040- and hardware-specific config */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 64
